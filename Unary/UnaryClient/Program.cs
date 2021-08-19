53bf66039f94db92cb5144f394f5669b43bf296f
LucasWBritz <lucas.wi.britz@gmail.com>
2021-08-18 21:22:41 -0300

    Create unary request

---
+++ Unary/UnaryClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace UnaryClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var grpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Calculator.CalculatorClient(grpcChannel);
+
+            var sumResult = await client.SumValueAsync(new Values() { A = 10, B = 20 });
+            Console.WriteLine($"10 + 20 = {sumResult.Result}");
+
+            var substractionResult = await client.SubtractValueAsync(new Values() { A = 10, B = 5 });
+            Console.WriteLine($"10 - 5 = {substractionResult.Result}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Unary/UnaryServer/Services/CalculatorService.cs
@@ -0,0 +1,35 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace UnaryServer.Services
+{
+    public class CalculatorService : Calculator.CalculatorBase
+    {
+        private readonly ILogger<CalculatorService> _logs;
+        public CalculatorService(ILogger<CalculatorService> logs)
+        {
+            _logs = logs;
+        }
+
+        public override Task<OperationResult> SumValue(Values request, ServerCallContext context)
+        {
+            _logs.LogDebug($"Adding values {request.A} + {request.B}");
+            
+            return Task.FromResult(new OperationResult()
+            {
+                Result = request.A + request.B
+            });
+        }
+
+        public override Task<OperationResult> SubtractValue(Values request, ServerCallContext context)
+        {
+            _logs.LogDebug($"Subtracting values {request.A} + {request.B}");
+
+            return Task.FromResult(new OperationResult()
+            {
+                Result = request.A - request.B
+            });
+        }
+    }
+}
